c8c63fb3f65b3ce9dd99722726d15b76f2062222
haradayoshiaki <y.team.kobe@gmail.com>
2020-12-05 20:52:45 +0900

    dev環境への対応

--- tuning-front/src/environments/environment.prod.ts
+++ tuning-front/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCrFBIkK0RUptEgEo4ebkqCqyrWZjVPZIU',
+    authDomain: 'tuning-a617f.firebaseapp.com',
+    projectId: 'tuning-a617f',
+    storageBucket: 'tuning-a617f.appspot.com',
+    messagingSenderId: '1077062906870',
+    appId: '1:1077062906870:web:01312397d9531796a4cdd7',
+    measurementId: 'G-07ZN148HGZ'
+  }
 };
