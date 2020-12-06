93cdb2007462726bcf491d52e8218d66822a38c7
haradayoshiaki <y.team.kobe@gmail.com>
2020-12-06 11:17:19 +0900

    ディレクトリ変更

---
+++ enviroments/tuning-front/build.sh
@@ -0,0 +1,23 @@
+## !/usr/bin/env bash
+set -e
+env=$1
+env=${env:-"dev"}
+echo "build ${env} mode"
+src=sources
+cwd=$(realpath $(dirname $0))
+project_root=$(git rev-parse --show-toplevel)
+
+rm -rf "${cwd}/${src}"
+echo "copy project.."
+cp -R "${project_root}/tuning-front" "${cwd}/${src}"
+cd "${cwd}/${src}"
+
+echo "build project.."
+npm install -g @angular/cli@11.0.1
+npm install
+npm audit fix
+ng build -c "${env}"
+
+echo "copy output to firebase public.."
+cp -R "${cwd}/${src}/dist/tuning-front/" "${project_root}/enviroments/firesbase/public"/
+rm -rf "${cwd}/${src}"
--- tuning-front/src/app/app.component.ts
+++ tuning-front/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tuning-front';
+  comments: Observable<Comment[]>;
+
+  constructor(private db: AngularFirestore) { }
+
+  ngOnInit(): void {
+    this.comments = this.db.collection<Comment>('comments').valueChanges();
+  }
 }
