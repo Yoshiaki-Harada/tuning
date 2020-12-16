f405e28ab9396dccd5b6af39b0cde7eeb9dc16b4
haradayoshiaki <y.team.kobe@gmail.com>
2020-12-16 17:03:11 +0900

    out put log

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -6,3 +6,6 @@ mkdir $GITHUB_WORKSPACE/secrets
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $GITHUB_WORKSPACE/secrets/serviceAccount.json serviceAccount.json.gpg
+cd $GITHUB_WORKSPACE/secrets
+ls
+
