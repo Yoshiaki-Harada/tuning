0266388937ab3b1b200448fec4e391a48347bf90
haradayoshiaki <y.team.kobe@gmail.com>
2020-12-16 16:57:18 +0900

    パスの指定方法の変更

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir $GITHUB_WORKSPACE/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/serviceAccount.json serviceAccount.json.gpg
+--output $GITHUB_WORKSPACE/secrets/serviceAccount.json serviceAccount.json.gpg
